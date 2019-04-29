ab2b6fbf0b29178d977fa6b00ef0e5863a4d9d69
caojilin <caojilin@berkeley.edu>
2019-04-29 14:37:09 -0700

    4

--- create_input_files.py
+++ create_input_files.py
@@ -3,7 +3,7 @@ from utils import create_input_files
 if __name__ == '__main__':
     # Create input files (along with word map)
     create_input_files(dataset='coco',
-                       karpathy_json_path='../data/andrej_split/dataset_coco.json',
+                       karpathy_json_path='../data/split/dataset_coco.json',
                        image_folder='../data',
                        captions_per_image=5,
                        min_word_freq=5,
