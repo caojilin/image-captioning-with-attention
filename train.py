0ee574e42f5b422eedd5ac6a0c41616c84eb1df5
caojilin <caojilin@berkeley.edu>
2019-05-02 10:28:03 -0700

    test

--- train.py
+++ train.py
@@ -36,7 +36,7 @@ best_bleu4 = 0.  # BLEU-4 score right now
 print_freq = 1  # print training/validation stats every __ batches
 fine_tune_encoder = False  # fine-tune encoder?
 # checkpoint = "BEST_checkpoint_coco_5_cap_per_img_5_min_word_freq.pth.tar"  # path to checkpoint, None if none
-checkpoint = False
+checkpoint = None
 
 def main():
     """
