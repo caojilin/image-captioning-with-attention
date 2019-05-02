afc2f12210a0e3baabfc1390222650b6aacae4d7
caojilin <caojilin@berkeley.edu>
2019-05-02 10:24:03 -0700

    test

--- create_input_files.py
+++ create_input_files.py
@@ -2,10 +2,10 @@ from utils import create_input_files
 
 if __name__ == '__main__':
     # Create input files (along with word map)
-    create_input_files(dataset='coco',
-                       karpathy_json_path='../data/split/dataset_coco.json',
-                       image_folder='../data',
+    create_input_files(dataset='flickr8k',
+                       karpathy_json_path='split/dataset_flickr8k.json',
+                       image_folder='datasets_8k/Flicker8k_Dataset',
                        captions_per_image=5,
                        min_word_freq=5,
-                       output_folder='../data/attention',
+                       output_folder='datasets_8k',
                        max_len=50)
