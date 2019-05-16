8506648f823ef60a10965bca57e7fca9c9a58949
caojilin <caojilin@berkeley.edu>
2019-05-15 21:56:25 -0700

    add vgg16

--- eval_files/simpler version/README.md
+++
@@ -1,6 +0,0 @@
-# image-captioning
-datasets: http://cocodataset.org/#download
-
-demo video : https://www.youtube.com/watch?v=EldYl3xzvqk&feature=youtu.be
-
-without attention, the simpler version trains faster
--- eval_files/simpler version/data/eval.py
+++
@@ -1,6 +0,0 @@
-from nlgeval import compute_metrics
-
-metrics_dict = compute_metrics(hypothesis='eval_files/hyp.txt',
-                               references=['eval_files/ref1.txt', 'eval_files/ref2.txt',
-                                           'eval_files/ref3.txt', 'eval_files/ref4.txt',
-                                           'eval_files/ref5.txt'])
